c9ed78c6e48d0a84ea874537e2b1624ecf1ee329
glitchrgamer <glitchrgamer@gmail.com>
2025-01-31 22:48:22 +0200

    frist1

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 const path = require('path')
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
   res.sendFile("./public/index.html",{  root:__dirname})
